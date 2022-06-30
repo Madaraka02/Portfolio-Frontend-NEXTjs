bd4cbf13a7c2f384ec6bdfe56c48709e67093cfd
Madaraka02 <victormadaraka@gmail.com>
2022-06-30 14:29:39 +0300

    added logo

--- components/Main.jsx
+++ components/Main.jsx
@@ -7,14 +7,15 @@ function Main() {
     <div className='w-full h-screen text-center'>
         <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
             <div>
-                <p className='uppercase tx-sm tracking-widest text-[#64ffda]'>WELCOME TO MY WORLD</p>
+                {/* <p className='uppercase tx-sm tracking-widest text-[#64ffda]'>WELCOME TO MY WORLD</p> */}
                 <p className='text-[#64ffda] tx-sm'>Hi, my name is</p>
                 <h2 className='py-4 text-[#e7e9ee]'>Victor Madaraka.</h2>
                 <h2 className='py-2 text-[#8892b0]'>
                         A FULL-STACK SOFTWARE <span className='text-[#64ffda]'>DEVELOPER</span>
                 </h2>
                 <p className='text-[#a8b2d1] py-4 max-w-[70%] m-auto'>
-                I'm a full stack developer with experience in designing, developing, and implementing applications and solutions using a range of technologies and programming languages. Seeking to leverage broad development experience and hands-on technical expertise in a challenging role as a Full-stack Developer.
+                I'm a full stack developer with experience in designing, developing, and implementing applications and solutions using a range of technologies and programming languages. 
+                Seeking to leverage broad development experience and hands-on technical expertise in a challenging role as a Full-stack Developer.
                 </p>
                 <div className='flex  items-center justify-between m-auto py-4 max-w-[330px]'>
                 <div className='rounded-full shadow-lg shadow-[#8892b0] p-3 cursor-pointer hover:scale-105 ease-in duration-500'>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -16,7 +16,7 @@ function Navbar() {
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
         <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-            <Image src="/" alt="" width='125' height='50' />
+            <Image src="/../public/assets/vicd.png" alt="" width='200' height='150' />
             <div>
                 <ul className='hidden md:flex'>
                     <Link href='/'>
