ceaf6e8dedac7c402ebc81421c90568459f63ab0
Madaraka02 <victormadaraka@gmail.com>
2022-09-12 16:13:57 +0300

    vercel fixes

--- components/Main.jsx
+++ components/Main.jsx
@@ -16,7 +16,7 @@ function Main() {
                         A FULL-STACK SOFTWARE <span className='text-[#64ffda]'>DEVELOPER</span>
                 </h2>
                 <p className='text-[#a8b2d1] py-4 max-w-[70%] m-auto'>
-                I'm a full stack developer with experience in designing, developing, and implementing applications and solutions using a range of technologies and programming languages. 
+                Im a full stack developer with experience in designing, developing, and implementing applications and solutions using a range of technologies and programming languages. 
                 Seeking to leverage broad development experience and hands-on technical expertise in a challenging role as a Full-stack Developer.
                 </p>
                 <div className='flex  items-center justify-between m-auto py-4 max-w-[330px]'>
